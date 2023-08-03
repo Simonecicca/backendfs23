43f8c4ee746e1d8ba77fc5d473600fc02f50a110
Simonecicca <129075136+Simonecicca@users.noreply.github.com>
2023-08-03 02:26:32 +0200

    esercizioMerGio

---
+++ Spring_MER_GIO/src/main/java/com/epicode/esercizi/SpringMerGioApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.esercizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMerGioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMerGioApplication.class, args);
+	}
+
+}
